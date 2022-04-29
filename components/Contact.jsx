764272e48640bbc9964dede948b421ed579dcbeb
Misqke <misqke.rust@yahoo.com>
2022-04-28 20:41:51 -0400

    contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
 import styles from "../styles/Contact.module.scss";
 
 const Contact = () => {
   return (
     <div id="contactContainer" className={styles.contactContainer}>
-      Contact
+      <div className={styles.contactContent}>
+        <h3>Introspective Images</h3>
+        <h4>Photogropher in New Castle, PA</h4>
+        <h4>
+          <a className={styles.email} href="mailto: moondogwp20@aol.com">
+            moondogwp20@aol.com
+          </a>
+        </h4>
+        <div className={styles.iconContainer}>
+          <a
+            className={`${styles.icon} ${styles.emailIcon}`}
+            href="mailto: moondogwp20@aol.com"
+          >
+            <EmailIcon fontSize="large" />
+          </a>
+          <a
+            className={`${styles.icon} ${styles.facebookIcon}`}
+            href="https://www.facebook.com/Moondogwp20/"
+            target="_blank"
+          >
+            <FacebookIcon fontSize="large" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
